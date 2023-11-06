f0cf6a33b6b4ba1bd53c43712d4f30a52ad2e79d
Thiago Falcão <thifalcao01@gmail.com>
2023-11-06 10:14:46 -0300

    Att banner 06/11

--- app/Views/pages/index/banner_header.php
+++ app/Views/pages/index/banner_header.php
@@ -3,14 +3,20 @@
     <div class="row g-0 align-items-center flex-column-reverse flex-lg-row">
         <div class="col-lg-12 wow fadeIn" data-wow-delay="0.5s">
             <div class="owl-carousel header-carousel">
+                <div class="owl-carousel-item focus position-relative">
+                    <img class="img-fluid" src="img/sitelabmovel.png" alt="">
+                </div>
                 <div class="owl-carousel-item position-relative">
                     <img class="img-fluid" src="img/Ativo-3.png" alt="">
                     <div class="owl-carousel-text">
                         <div class="col-lg-12 col-12 text-center wow fadeIn" data-wow-delay="0.5s">
-                            <h1 class="d-inline-block border rounded-pill text-primary bg-light py-1 px-4"> Para realização dos seus exames.<br>A LabMovel vai até você! </h1>
+                            <h1 class="d-inline-block border rounded-pill text-primary bg-light py-1 px-4"> Para
+                                realização dos seus exames.<br>A LabMovel vai até você! </h1>
                             <p class="display-5 text-white">Faça o seu agendamento online<br>
                                 De forma rápida e segura! </p>
-                            <a class="btn btn-outline-dark rounded-pill py-3 px-5 mt-3" href="https://api.whatsapp.com/send?phone=5511973913314" target="_blank">Agende seus exames! <i class="fa-solid fa-heart-pulse me-2 fs-5"></i></a>
+                            <a class="btn btn-outline-dark rounded-pill py-3 px-5 mt-3"
+                                href="https://api.whatsapp.com/send?phone=5511973913314" target="_blank">Agende seus
+                                exames! <i class="fa-solid fa-heart-pulse me-2 fs-5"></i></a>
                         </div>
                     </div>
                 </div>
@@ -18,10 +24,14 @@
                     <img class="img-fluid" src="img/Ativo-2-2.png" alt="">
                     <div class="owl-carousel-text">
                         <div class="col-lg-12 col-12 text-center wow fadeIn" data-wow-delay="0.5s">
-                            <h1 class="d-inline-block border rounded-pill text-primary bg-light py-1 px-4">Atendimento Domiciliar</h1>
+                            <h1 class="d-inline-block border rounded-pill text-primary bg-light py-1 px-4">Atendimento
+                                Domiciliar</h1>
                             <h4 class="display-5 text-white py-1 px-4">Humanizado & Particular </h4>
-                            <p class="display-5 text-white text-center fs-5">Você irá realizar exames laboratoriais de check-up completo</p>
-                            <a class="btn btn-outline-dark rounded-pill py-3 px-5 mt-3" href="https://api.whatsapp.com/send?phone=5511973913314" target="_blank">Agende seus exames! <i class="fa-solid fa-heart-pulse me-2 fs-5"></i></a>
+                            <p class="display-5 text-white text-center fs-5">Você irá realizar exames laboratoriais de
+                                check-up completo</p>
+                            <a class="btn btn-outline-dark rounded-pill py-3 px-5 mt-3"
+                                href="https://api.whatsapp.com/send?phone=5511973913314" target="_blank">Agende seus
+                                exames! <i class="fa-solid fa-heart-pulse me-2 fs-5"></i></a>
                         </div>
                     </div>
                 </div>
@@ -29,13 +39,17 @@
                     <img class="img-fluid" src="img/4.png" alt="">
                     <div class="owl-carousel-text">
                         <div class="text-primary col-lg-12 col-12 text-center wow fadeIn" data-wow-delay="0.5s">
-                            <h1 class="d-inline-block border rounded-pill text-primary bg-light py-1 px-4">Exames & Testes de Alergia</h1><br>
-                            <h6 class="display-6 text-white">Disponibilizamos mais de 3000 exames<br> e mais de 90 testes de alergias. </h6>
+                            <h1 class="d-inline-block border rounded-pill text-primary bg-light py-1 px-4">Exames &
+                                Testes de Alergia</h1><br>
+                            <h6 class="display-6 text-white">Disponibilizamos mais de 3000 exames<br> e mais de 90
+                                testes de alergias. </h6>
                             <p class="text-white display-5 text-center fs-5">Utilizamos tecnologia avançada nos
                                 resultados dos seus exames.<br>
                                 Proporcionamos aos nossos clientes
                                 conforto nas práticas de atendimento. </p>
-                                <a class="btn btn-outline-dark rounded-pill py-3 px-5 mt-3" href="https://api.whatsapp.com/send?phone=5511973913314" target="_blank">Agende seus exames! <i class="fa-solid fa-heart-pulse me-2 fs-5"></i></a>
+                            <a class="btn btn-outline-dark rounded-pill py-3 px-5 mt-3"
+                                href="https://api.whatsapp.com/send?phone=5511973913314" target="_blank">Agende seus
+                                exames! <i class="fa-solid fa-heart-pulse me-2 fs-5"></i></a>
                         </div>
                     </div>
                 </div>
